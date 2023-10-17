df4a78fe9786c56e6a5beb79d26b3a30d4dfb4bb
RArielAlonso <rariel.alonso@gmail.com>
2023-10-17 09:56:00 -0300

    agregado de transform - generacion de dataframes

--- dags/etl_modules/transform.py
+++ dags/etl_modules/transform.py
@@ -9,7 +9,7 @@ logging.basicConfig(format="%(asctime)s - %(filename)s - %(message)s", level=log
 request_paths = extract()
 
 
-def transform_dataframe_weather():
+def transform_weather():
     logging.info("Start transforming the weather dataframe")
     data_clouds = pd.json_normalize(load_json(request_paths['weather'])['weather'])
     data_temperature = pd.json_normalize(load_json(request_paths['weather'])['main'])
@@ -25,12 +25,31 @@ def transform_dataframe_weather():
     return data_weather
 
 
+def transform_system_info():
+    data_system_info = pd.json_normalize(load_json(request_paths['system_info_eco_bikes']))
+    data_system_info['last_updated'] = pd.to_datetime(data_system_info['last_updated']-10400, unit='s')
+    return data_system_info
+
+
+def transform_station_status():
+    data_station_status = pd.DataFrame.from_dict(pd.json_normalize(load_json(request_paths['station_status_eco_bikes']))['data.stations'][0])
+    data_station_status['last_reported'] = pd.to_datetime(data_station_status['last_reported']-10400, unit='s')
+    data_station_status.drop(['num_bikes_available_types', 'is_charging_station', 'traffic'], axis=1, inplace=True)
+    return data_station_status
+
+
+def transform_station_info():
+    data_station_info = pd.DataFrame.from_dict(pd.json_normalize(load_json(request_paths['station_info_eco_bikes']))['data.stations'][0])
+    return data_station_info
+
+
 def transform():
     create_dim_date_table()
+    # transform_weather()
+    # transform_system_info()
+    # transform_station_status()
+    transform_station_info()
 
 
 if __name__ == "__main__":
-    data = transform_dataframe_weather()
-    print(data)
-    # data.to_csv("asd.csv")
-    # transform()
+    transform()
