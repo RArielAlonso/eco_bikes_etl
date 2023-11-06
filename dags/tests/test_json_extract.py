323ed9ba6de1c58e48b50cb11e6dc04ca9529950
RArielAlonso <rariel.alonso@gmail.com>
2023-11-06 10:24:06 -0300

    added tests

--- dags/tests/test_json_extract.py
+++ dags/tests/test_json_extract.py
@@ -1,18 +1,11 @@
-from etl_modules.extract import extract as my_module
+from utils import utils as my_module
 from unittest.mock import patch
-from config.config import extract_list
+from config.config import weather_ds
 
 
-@patch('etl_modules.extract.extract')
-def test_json_paths_from_apis(mock_get):
-    mock_get.return_value.json.return_value = {'weather': '/tmp/weather.json',
-                                               'system_info_eco_bikes': '/tmp/system_info_eco_bikes.json',
-                                               'station_status_eco_bikes': '/tmp/station_status_eco_bikes.json',
-                                               'station_info_eco_bikes': '/tmp/station_info_eco_bikes.json'}
-    excepted_output = {'weather': '/tmp/weather.json',
-                       'system_info_eco_bikes': '/tmp/system_info_eco_bikes.json',
-                       'station_status_eco_bikes': '/tmp/station_status_eco_bikes.json',
-                       'station_info_eco_bikes': '/tmp/station_info_eco_bikes.json'}
-    actual_output = my_module(extract_list)
-
-    assert excepted_output == actual_output, f"expected value:{excepted_output} differs from actual value {actual_output}"
+@patch('utils.utils.get_request_json')
+def test_json_from_apis(mock_get):
+    mock_get.return_value = {'coord': 10}
+    data = my_module.get_request_json(weather_ds['base_url'], weather_ds['params'])
+    excepted_output = {'coord': 10}
+    assert excepted_output == data, f"expected value:{excepted_output} differs from actual value {data}"
